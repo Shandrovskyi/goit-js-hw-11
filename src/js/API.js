6b665dd99c4719bbe24b8a5ee62e8b83f4488fdc
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-05-24 17:14:03 +0300

    Update API.js

--- src/js/API.js
+++ src/js/API.js
@@ -3,7 +3,7 @@ import axios from "axios"; // для здійснення HTTP запитів
 import { Notify } from 'notiflix/build/notiflix-notify-aio'; // для відображення повідомлень
 
 // Оголошення констант
-const API_KEY = "36502661-e8ee83efff2e99e0261d33261";
+const API_KEY = "35924143-9020fc77f3274be39114409f4";
 const URL = "https://pixabay.com/api/";
 const quantityPage = 40;
 
